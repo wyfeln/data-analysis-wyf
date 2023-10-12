1727d73449cf2f22a5486e7ba8351844c2a2d105
wyfeln <118720398+wyfeln@users.noreply.github.com>
2023-10-12 14:29:05 +0800

    Add files via upload

---
+++ Part I Coding Pythonically/Python Basics/module_max.py
@@ -0,0 +1,10 @@
+def funcMax(a,b):#自定义函数求a，b之间的最大值
+    if a>b:
+        return a
+    else:
+        return b
+def funcMin(a,b):#自定义函数求a，b之间的最少值
+    if a<b:
+        return a
+    else:
+        return b
